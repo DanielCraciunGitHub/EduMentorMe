f09878dd400bd228f57b50f0fe9cc5be10980fd8
Daniel <danielcraciun111@yahoo.com>
2023-06-23 18:58:59 +0100

    template code v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <h1 className="text-4xl w-full bg-slate-700 p-3 flex justify-center">
+      Template
+    </h1>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
