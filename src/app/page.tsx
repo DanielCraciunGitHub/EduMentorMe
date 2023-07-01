f5ad6f3066936fcb5870d6b0c2486ea1164b96e1
Daniel <danielcraciun111@yahoo.com>
2023-07-01 09:28:19 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ const page: FC = () => {
   return (
     <div className="flex-grow flex flex-col items-center justify-center space-y-4 overflow-y-auto">
       <SearchForm />
+      <div>test</div>
     </div>
   )
 }
