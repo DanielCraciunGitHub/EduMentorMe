980d331f17bfebc49e3ed88bc2310fc4bc58f907
Daniel <danielcraciun111@yahoo.com>
2023-07-03 15:18:24 +0100

    session data accessible

--- src/app/(Navigation)/account/page.tsx
+++ src/app/(Navigation)/account/page.tsx
@@ -1,24 +1,38 @@
 "use client"
 
 import { Button } from "@/app/components/ui/button"
-import type { Database } from "@/types/supabase"
+import type { Database, Row } from "@/types/supabase"
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import { useRouter } from "next/navigation"
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 
 const page: FC = () => {
   const supabase = createClientComponentClient<Database>()
+
   const router = useRouter()
+  const [user, setUser] = useState<Row | null>(null)
+
+  useEffect(() => {
+    const handleSetUser = async () => {
+      const { data } = await supabase.from("users").select().single()
+      setUser(data)
+    }
+    handleSetUser()
+  }, [])
 
   const handleSignOut = async () => {
     await supabase.auth.signOut()
     router.refresh()
   }
-
   return (
-    <Button type="submit" onClick={handleSignOut}>
-      Sign out
-    </Button>
+    <div className="flex flex-col items-center">
+      <div>{user?.email}</div>
+      <div>
+        <Button type="submit" onClick={handleSignOut}>
+          Sign out
+        </Button>
+      </div>
+    </div>
   )
 }
 
