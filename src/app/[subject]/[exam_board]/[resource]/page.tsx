726b5d5037b48682fae8fc9dbf2d5a163a29e310
Daniel <danielcraciun111@yahoo.com>
2023-07-04 20:05:02 +0100

    files can be displayed

--- src/app/[subject]/[exam_board]/[resource]/page.tsx
+++ src/app/[subject]/[exam_board]/[resource]/page.tsx
@@ -1,10 +1,42 @@
-import { FC } from "react"
+"use client"
 
-interface pageProps {
+import { FC, useEffect, useState } from "react"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+
+interface PageProps {
   params: { subject: string; exam_board: string; resource: string }
 }
 
-const page: FC<pageProps> = ({ params }) => {
-  return params.resource
+const Page: FC<PageProps> = ({ params }) => {
+  const [blobUrl, setBlobUrl] = useState<string | null>(null)
+  const supabase = createClientComponentClient()
+
+  useEffect(() => {
+    const fetchFile = async () => {
+      const { data } = await supabase.storage
+        .from("files")
+        .download("computer-science/Resume.pdf")
+
+      if (data) {
+        const url = URL.createObjectURL(data)
+        setBlobUrl(url)
+      }
+    }
+
+    fetchFile()
+  }, [])
+
+  return (
+    <div>
+      {blobUrl ? (
+        <a href={blobUrl} target="_blank" rel="noopener noreferrer">
+          Open file
+        </a>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  )
 }
-export default page
+
+export default Page
