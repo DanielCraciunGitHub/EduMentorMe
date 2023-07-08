82263fddc3e586570b508580c2325845ae0e66c4
Daniel <danielcraciun111@yahoo.com>
2023-07-08 20:01:19 +0100

    Made the login and sign up forms look nicer + code cleaner

--- src/app/(Navigation)/login/page.tsx
+++ src/app/(Navigation)/login/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { FC } from "react"
 import { useAuthState } from "@/app/components/hooks/useAuthState"
 
 import { useForm } from "react-hook-form"
@@ -18,7 +17,6 @@ import {
   FormMessage,
 } from "@/app/components/ui/form"
 import { Input } from "@/app/components/ui/input"
-import { Card } from "@/app/components/ui/card"
 import { Alert, AlertDescription, AlertTitle } from "@/app/components/ui/alert"
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 
@@ -29,7 +27,7 @@ const formSchema = z.object({
   password: z.string().min(6, { message: "Invalid Password" }),
 })
 
-const page: FC = () => {
+const page = () => {
   const supabase = createClientComponentClient<Database>()
 
   const { router, isError, setIsError } = useAuthState(false)
@@ -39,82 +37,72 @@ const page: FC = () => {
   })
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    // runs custom made postgres function that returns a boolean value
-    const { data: exists } = await supabase.rpc("email_exists", {
-      email_param: values.email,
+    const { error } = await supabase.auth.signInWithPassword({
+      email: values.email,
+      password: values.password,
     })
-    if (!exists) {
+
+    if (error) {
       setIsError(true)
     } else {
-      const { error } = await supabase.auth.signInWithPassword({
-        email: values.email,
-        password: values.password,
-      })
-
-      if (error) {
-        setIsError(true)
-      } else {
-        router.push("/")
-        router.refresh()
-      }
+      router.push("/")
+      router.refresh()
     }
   }
 
   return (
-    <Card className="flex w-full items-center justify-center">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col space-y-8 w-1/2"
-        >
-          <div className="space-y-2">
-            <h1 className="text-4xl">Login</h1>
-            <p className="font-light">
-              Join a fast growing community of successful students
-            </p>
-          </div>
-          {isError ? (
-            <Alert variant="destructive">
-              <AlertTitle>Error</AlertTitle>
-              <AlertDescription>
-                1. Either Account doesn't exist <br />
-                2. Or password is incorrect
-              </AlertDescription>
-            </Alert>
-          ) : null}
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="johndoe@gmail.com" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormDescription>
-                  Must be at least 6 characters long
-                </FormDescription>
-                <FormControl>
-                  <Input placeholder="johndoe0!" type="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Login</Button>
-        </form>
-      </Form>
-    </Card>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col space-y-8 w-1/2 md:w-1/3 justify-center"
+      >
+        <div className="space-y-2">
+          <h1 className="text-4xl">Login</h1>
+          <p className="font-light">
+            Join a fast growing community of successful students
+          </p>
+        </div>
+        {isError ? (
+          <Alert variant="destructive">
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>
+              1. Either Account doesn't exist <br />
+              2. Or password is incorrect
+            </AlertDescription>
+          </Alert>
+        ) : null}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="johndoe@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormDescription>
+                Must be at least 6 characters long
+              </FormDescription>
+              <FormControl>
+                <Input placeholder="johndoe0!" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Login</Button>
+      </form>
+    </Form>
   )
 }
 
--- src/app/(Navigation)/sign_up/page.tsx
+++ src/app/(Navigation)/sign_up/page.tsx
@@ -19,7 +19,6 @@ import {
   FormMessage,
 } from "@/app/components/ui/form"
 import { Input } from "@/app/components/ui/input"
-import { Card } from "@/app/components/ui/card"
 import { Alert, AlertDescription, AlertTitle } from "@/app/components/ui/alert"
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import type { Database } from "@/types/supabase"
@@ -45,7 +44,6 @@ const page: FC = () => {
     })
     // if no email present in database
     if (!exists) {
-      // Takes around 1 second to complete due to advanced functions
       await supabase.auth.signUp({
         email: values.email,
         password: values.password,
@@ -64,83 +62,81 @@ const page: FC = () => {
   }
 
   return (
-    <Card className="flex w-full items-center justify-center">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col space-y-8 w-1/2"
-        >
-          <div className="space-y-2">
-            <h1 className="text-4xl">Sign Up</h1>
-            <p className="font-light">
-              Join a fast growing community of successful students
-            </p>
-          </div>
-          {isError ? (
-            <Alert variant="destructive">
-              <AlertTitle>Error</AlertTitle>
-              <AlertDescription>
-                An account is already registered to this email, consider{" "}
-                <Link href="/login" className="underline text-blue-500">
-                  logging in
-                </Link>
-              </AlertDescription>
-            </Alert>
-          ) : null}
-          {isEmailVerify ? (
-            <Alert variant="default" className="bg-green-500 dark:bg-green-700">
-              <AlertTitle>Success</AlertTitle>
-              <AlertDescription>
-                Check your email inbox to verify your account
-              </AlertDescription>
-            </Alert>
-          ) : null}
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="john doe" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="johndoe@gmail.com" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormDescription>
-                  Must be at least 6 characters long
-                </FormDescription>
-                <FormControl>
-                  <Input placeholder="johndoe0!" type="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Sign Up</Button>
-        </form>
-      </Form>
-    </Card>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col space-y-8 w-1/2 md:w-1/3 justify-center"
+      >
+        <div className="space-y-2">
+          <h1 className="text-4xl">Sign Up</h1>
+          <p className="font-light">
+            Join a fast growing community of successful students
+          </p>
+        </div>
+        {isError ? (
+          <Alert variant="destructive">
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>
+              An account is already registered to this email, consider{" "}
+              <Link href="/login" className="underline text-blue-500">
+                logging in
+              </Link>
+            </AlertDescription>
+          </Alert>
+        ) : null}
+        {isEmailVerify ? (
+          <Alert variant="default" className="bg-green-500 dark:bg-green-700">
+            <AlertTitle>Success</AlertTitle>
+            <AlertDescription>
+              Check your email inbox to verify your account
+            </AlertDescription>
+          </Alert>
+        ) : null}
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="john doe" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="johndoe@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormDescription>
+                Must be at least 6 characters long
+              </FormDescription>
+              <FormControl>
+                <Input placeholder="johndoe0!" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Sign Up</Button>
+      </form>
+    </Form>
   )
 }
 
