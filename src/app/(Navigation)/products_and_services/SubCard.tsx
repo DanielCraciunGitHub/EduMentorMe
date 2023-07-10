116cd797171b5d3f042e52608211f4fa7a05c1aa
Daniel <danielcraciun111@yahoo.com>
2023-07-10 18:54:47 +0100

    working reusable subcard component

--- src/app/(Navigation)/products_and_services/SubCard.tsx
+++ src/app/(Navigation)/products_and_services/SubCard.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/app/components/ui/button"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -15,12 +14,13 @@ import { FC } from "react"
 
 interface SubCardProps {
   title: string
+  theme: "green" | "orange" | "red"
   features: string[]
   price: string
   icon: React.ReactNode
 }
 
-const SubCard: FC<SubCardProps> = ({ title, features, price, icon }) => {
+const SubCard: FC<SubCardProps> = ({ title, features, price, icon, theme }) => {
   return (
     <Card className="w-[20rem] flex flex-col flex-grow space-y-2">
       <CardHeader className="space-y-4">
@@ -28,24 +28,27 @@ const SubCard: FC<SubCardProps> = ({ title, features, price, icon }) => {
           <CardTitle className="flex justify-center">{title}</CardTitle>
           {icon}
         </div>
-        <Separator className="h-[5px] bg-orange-500 dark:bg-orange-500 rounded" />
+        <Separator
+          className={`h-[5px] bg-${theme}-500 dark:bg-${theme}-500 rounded`}
+        />
       </CardHeader>
       <CardContent className="space-y-3">
         {features.map((feature) => (
-          <div className="flex flex-row space-x-7">
-            <CheckCircle className="text-lime-400" />
-            <li key={feature} className="list-none">
-              {feature}
-            </li>
+          <div key={feature} className="space-x-5 flex">
+            <CheckCircle className="text-lime-400 flex-none" />
+            <div>{feature}</div>
           </div>
         ))}
       </CardContent>
       <CardFooter className="flex flex-col space-y-4">
         <div className="flex flex-row items-end">
-          <h1 className="text-orange-500">{`£${price}`}</h1>
+          <h1 className={`text-${theme}-500`}>{price}</h1>
           <p className="text-xs">/Month</p>
         </div>
-        <Button type="submit" className="bg-orange-500 dark:bg-orange-500">
+        <Button
+          type="submit"
+          className={`bg-${theme}-500 dark:bg-${theme}-500`}
+        >
           Purchase
         </Button>
       </CardFooter>
--- src/app/(Navigation)/products_and_services/page.tsx
+++ src/app/(Navigation)/products_and_services/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next"
 import SubCard from "./SubCard"
 import { Separator } from "@/app/components/ui/separator"
-import { Rocket } from "lucide-react"
+import { Crown, Rocket, Sun } from "lucide-react"
 
 export const metadata: Metadata = {
   title: "EduMentorMe | Products And Services",
@@ -12,14 +12,29 @@ export const metadata: Metadata = {
 function page() {
   return (
     <div className="flex flex-col space-y-8 items-center">
-      <div className="text-3xl">Test title</div>
+      <div className="text-3xl">Products & Services</div>
       <Separator />
-      <div className="flex flex-col md:flex-row justify-center items-center gap-5">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-5 flex-wrap">
         <SubCard
-          icon={<Rocket className="text-orange-500" />}
+          icon={<Sun className="text-yellow-500" />}
+          theme="green"
           title="Basic Plan"
           features={["this feature", "that feature", "xyz feature"]}
-          price="19.99"
+          price="£19.99"
+        />
+        <SubCard
+          icon={<Rocket className="text-orange-500" />}
+          theme="orange"
+          title="Advanced Plan"
+          features={["this feature", "that feature", "xyz feature"]}
+          price="£39.99"
+        />
+        <SubCard
+          icon={<Crown className="text-yellow-500" />}
+          theme="red"
+          title="Master Plan"
+          features={["this feature", "that feature", "xyz feature"]}
+          price="£99.99"
         />
       </div>
     </div>
