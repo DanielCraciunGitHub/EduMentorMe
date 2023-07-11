aafadbf6470bdc55a61012278d8591c3b49a0542
Daniel <danielcraciun111@yahoo.com>
2023-07-11 19:20:50 +0100

    testing

--- src/app/components/providers.tsx
+++ src/app/components/providers.tsx
@@ -1,11 +1,8 @@
 "use client"
 
-import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
-import { useUserListener } from "./hooks/useUserListener"
 
 export function Provider({ children, ...props }: ThemeProviderProps) {
-  const listener = useUserListener() // temp solution
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
