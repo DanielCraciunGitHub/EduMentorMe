36deb82e3c75839272bad27c769e5c658dd8d334
Daniel <danielcraciun111@yahoo.com>
2023-07-12 20:17:46 +0100

    fixed initial loading UI for mobile

--- src/app/components/MobileBar.tsx
+++ src/app/components/MobileBar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { NavItem } from "@/app/components/NavItem"
 import { useUserStore } from "./hooks/useUserStore"
 import { PanelRight } from "lucide-react"
@@ -11,55 +13,34 @@ import {
 export function MobileBar() {
   const { user } = useUserStore()
   return (
-    <div className="flex flex-col items-center py-2">
+    <div className="flex flex-col items-center py-2 md:hidden">
       <NavItem key="EMM" page="/" text="EMM" className="text-4xl" />
       <Sheet>
         <SheetTrigger className="flex w-full justify-end p-2">
           <PanelRight className="dark:text-slate-300" />
         </SheetTrigger>
         <SheetContent className="flex flex-col items-center" side="right">
+          <SheetClose asChild>
+            <NavItem key="/about_us" page="/about_us" text="About Us" />
+          </SheetClose>
+          <SheetClose asChild>
+            <NavItem key="/contact_us" page="/contact_us" text="Contact Us" />
+          </SheetClose>
+          <SheetClose asChild>
+            <NavItem
+              key="/products_and_services"
+              page="/products_and_services"
+              text="Products And Services"
+            />
+          </SheetClose>
           {user ? (
             <>
-              <SheetClose asChild>
-                <NavItem key="/about_us" page="/about_us" text="About Us" />
-              </SheetClose>
-              <SheetClose asChild>
-                <NavItem
-                  key="/contact_us"
-                  page="/contact_us"
-                  text="Contact Us"
-                />
-              </SheetClose>
-              <SheetClose asChild>
-                <NavItem
-                  key="/products_and_services"
-                  page="/products_and_services"
-                  text="Products And Services"
-                />
-              </SheetClose>
               <SheetClose asChild>
                 <NavItem key="/account" page="/account" text="Account" />
               </SheetClose>
             </>
           ) : (
             <>
-              <SheetClose asChild>
-                <NavItem key="/about_us" page="/about_us" text="About Us" />
-              </SheetClose>
-              <SheetClose asChild>
-                <NavItem
-                  key="/contact_us"
-                  page="/contact_us"
-                  text="Contact Us"
-                />
-              </SheetClose>
-              <SheetClose asChild>
-                <NavItem
-                  key="/products_and_services"
-                  page="/products_and_services"
-                  text="Products And Services"
-                />
-              </SheetClose>
               <SheetClose asChild>
                 <NavItem key="/login" page="/login" text="Login" />
               </SheetClose>
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,10 +1,11 @@
-"use client"
-
-import useResize from "@/app/components/hooks/useResize"
 import { MobileBar } from "@/app/components/MobileBar"
 import { DesktopBar } from "@/app/components/DesktopBar"
 
 export default function NavBar() {
-  const { isMobile } = useResize(768)
-  return <nav>{isMobile ? <MobileBar /> : <DesktopBar />}</nav>
+  return (
+    <nav>
+      <MobileBar />
+      <DesktopBar />
+    </nav>
+  )
 }
