6282f02740fedaf54703c1276175e7a84bd8bccf
Daniel <danielcraciun111@yahoo.com>
2023-07-14 19:14:06 +0100

    small changes

---
+++ src/config/site.ts
@@ -0,0 +1,37 @@
+import type { FooterButton, NavItem } from "@/types"
+
+const links = {}
+
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Edu Mentor Me",
+  description: "Learn GCSEs and A-Levels with us for free",
+  url: "https://edumentorme.com",
+  mainNav: [
+    {
+      name: "EMM",
+      href: "/",
+    },
+    {
+      name: "About Us",
+      href: "/about_us",
+    },
+    {
+      name: "Contact Us",
+      href: "/contact_us",
+    },
+    {
+      name: "Products And Services",
+      href: "/products_and_services",
+    },
+    {
+      name: "Login",
+      href: "/login",
+    },
+    {
+      name: "Sign Up",
+      href: "/sign_up",
+    },
+  ] satisfies NavItem[],
+}
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface NavItem {
+  name: string
+  href: string
+}
+export interface FooterButton {
+  icon: React.ReactNode
+  href: string
+}
