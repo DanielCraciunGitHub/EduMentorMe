0100691df77eb07b906fed942e9060dcd85a061f
Daniel <danielcraciun111@yahoo.com>
2023-08-09 08:37:23 +0100

    fixed metadata for main page

--- src/app/(features)/page.tsx
+++ src/app/(features)/page.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from "next"
 import { notFound } from "next/navigation"
 import { allFeatures } from "contentlayer/generated"
 
+import { baseMetadata } from "@/config/meta"
 import { Mdx } from "@/components/Mdx"
+
+export const metadata: Metadata = {
+  ...baseMetadata,
+}
 
 const page = () => {
   const feature = allFeatures.find((feature) => feature.title === "EduMentorMe")
