040c264c9a7e203be44232b311140fb814cdbcaa
Daniel <danielcraciun111@yahoo.com>
2023-08-13 20:02:40 +0100

    upgraded timer functionality

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -6,24 +6,15 @@ import { useTimer } from "react-timer-hook"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
-interface Time {
-  hours: number
-  minutes: number
-  seconds: number
-}
-
 export function Timer() {
-  const [inputValue, setInputValue] = useState("")
+  const [hoursInput, setHours] = useState("")
+  const [minutesInput, setMinutes] = useState("")
+  const [secondsInput, setSeconds] = useState("")
+
   const [toggleText, setToggleText] = useState<"Start" | "Pause">("Start")
   const [CountdownColor, setCountDownColor] = useState<"text-red-500" | "">("")
 
   const alarmRef = useRef<HTMLAudioElement | undefined>()
-  useEffect(() => {
-    const alarm = new Audio("/timer-alarm.mp3")
-    alarmRef.current = alarm
-
-    return () => alarmRef.current?.remove()
-  }, [])
 
   const { seconds, minutes, hours, isRunning, resume, pause, restart } =
     useTimer({
@@ -36,31 +27,62 @@ export function Timer() {
       },
     })
 
-  const changeTime = (value: string): void => {
-    const validInput = validateInput(value)
+  // Defines the alarm object instance and reference on component mount
+  useEffect(() => {
+    const alarm = new Audio("/timer-alarm.mp3")
+    alarmRef.current = alarm
 
-    if (!validInput) return
+    return () => alarmRef.current?.remove()
+  }, [])
 
-    setInputValue(value)
+  // changes the timer state whenever the hours, minutes or seconds input changes
+  useEffect(() => {
+    restart(getTimeStamp(), false)
+  }, [hoursInput, minutesInput, secondsInput])
 
-    const newTimeStamp = createNewTimeStamp(validInput)
+  // Makes sure the timer state is visible in the browser tab
+  useEffect(() => {
+    if (isRunning) {
+      const formattedHours = String(hours).padStart(2, "0")
+      const formattedMinutes = String(minutes).padStart(2, "0")
+      const formattedSeconds = String(seconds).padStart(2, "0")
 
-    restart(newTimeStamp, false)
+      document.title = `${formattedHours}:${formattedMinutes}:${formattedSeconds} | EduMentorMe`
+    } else {
+      document.title = "Study Timer | EduMentorMe"
+    }
+  }, [isRunning, seconds])
+
+  const changeTime = (field: string, value: string): void => {
+    if (!validateInput(field, value)) return
+
+    if (field === "hours") {
+      setHours(value)
+    } else if (field === "minutes") {
+      setMinutes(value)
+    } else if (field === "seconds") {
+      setSeconds(value)
+    }
   }
 
-  const validateInput = (value: string): Time | undefined => {
-    const hours = Number(value.substring(0, 2))
-    const minutes = Number(value.substring(2, 4))
-    const seconds = Number(value.substring(4, 6))
+  const validateInput = (field: string, value: string): boolean => {
+    // The regex is used to limit the input to a max of two integers
+    const isValid =
+      ((field === "hours" && Number(value) < 24) ||
+        (field !== "hours" && Number(value) < 60)) &&
+      /^\d{0,2}$/.test(value)
 
-    const validInput =
-      /^\d{0,6}$/.test(value) && hours < 24 && minutes < 60 && seconds < 60
-
-    if (validInput) return { hours, minutes, seconds }
+    return isValid
   }
 
-  const createNewTimeStamp = ({ hours, minutes, seconds }: Time): Date => {
-    const totalSeconds = hours * 3600 + minutes * 60 + seconds
+  const getTimeStamp = (): Date => {
+    // Extracts raw seconds from stateful inputs then creates a timestamp
+    // for those seconds in the future, used to display the correct timer UI.
+    const totalSeconds =
+      Number(hoursInput) * 3600 +
+      Number(minutesInput) * 60 +
+      Number(secondsInput)
+
     const newTimeStamp = new Date(Date.now() + totalSeconds * 1000)
 
     return newTimeStamp
@@ -68,15 +90,11 @@ export function Timer() {
 
   const CountdownText = (): React.JSX.Element => (
     <div className={`text-6xl md:text-9xl ${CountdownColor}`}>
-      <span>{formatCountdownUI(hours)}</span>:
-      <span>{formatCountdownUI(minutes)}</span>:
-      <span>{formatCountdownUI(seconds)}</span>
+      <span>{String(hours).padStart(2, "0")}</span>:
+      <span>{String(minutes).padStart(2, "0")}</span>:
+      <span>{String(seconds).padStart(2, "0")}</span>
     </div>
   )
-
-  const formatCountdownUI = (value: number): string => {
-    return value.toString().padStart(2, "0")
-  }
 
   const toggleTimer = (): void => {
     if (isRunning) {
@@ -89,16 +107,23 @@ export function Timer() {
   }
 
   const resetTimer = (): void => {
-    const time = new Date()
+    // resets timer state back to what the user sets
+    restart(getTimeStamp(), false)
 
-    restart(time, false)
-
+    // resets the audio state
     alarmRef.current?.pause()
     alarmRef.current!.currentTime = 0
 
     setToggleText("Start")
-    setInputValue("")
     setCountDownColor("")
+  }
+
+  const clearTimer = (): void => {
+    if (!isRunning) {
+      setHours("")
+      setMinutes("")
+      setSeconds("")
+    }
   }
 
   return (
@@ -106,12 +131,34 @@ export function Timer() {
       <div
         className={isRunning || CountdownColor !== "" ? "hidden" : "visible"}
       >
-        <Input
-          value={inputValue}
-          onChange={(e) => changeTime(e.target.value)}
-          placeholder="hhmmss"
-          className="text-center text-2xl"
-        />
+        <div className="flex justify-center">
+          <div className="flex w-3/4 space-x-4 md:w-1/3">
+            <Input
+              key="hours"
+              type="tel"
+              value={hoursInput}
+              onChange={(e) => changeTime("hours", e.target.value)}
+              placeholder="hh"
+              className="text-center text-xl"
+            />
+            <Input
+              key="minutes"
+              type="tel"
+              value={minutesInput}
+              onChange={(e) => changeTime("minutes", e.target.value)}
+              placeholder="mm"
+              className="text-center text-xl"
+            />
+            <Input
+              key="seconds"
+              type="tel"
+              value={secondsInput}
+              onChange={(e) => changeTime("seconds", e.target.value)}
+              placeholder="ss"
+              className="text-center text-xl"
+            />
+          </div>
+        </div>
         <p className="text-gray-500 dark:text-gray-500">Max: 23:59:59</p>
       </div>
       <CountdownText />
@@ -127,6 +174,12 @@ export function Timer() {
           className="focus:bg-orange-500 dark:focus:bg-orange-500"
         >
           Reset
+        </Button>
+        <Button
+          onClick={clearTimer}
+          className="focus:bg-orange-500 dark:focus:bg-orange-500"
+        >
+          Clear
         </Button>
       </div>
     </div>
